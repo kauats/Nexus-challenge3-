61261621eb21275c58d1c7268c71e98cb00aec10
kauats <kauat.santos12@gmail.com>
2024-07-30 14:48:39 -0300

    Initial Commit

---
+++ Nexus(challenge3)/Nexus_challenge3_App.swift
@@ -0,0 +1,17 @@
+//
+//  Nexus_challenge3_App.swift
+//  Nexus(challenge3)
+//
+//  Created by Kaua Trindade on 30/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Nexus_challenge3_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Nexus(challenge3)UITests/Nexus_challenge3_UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Nexus_challenge3_UITestsLaunchTests.swift
+//  Nexus(challenge3)UITests
+//
+//  Created by Kaua Trindade on 30/07/24.
+//
+
+import XCTest
+
+final class Nexus_challenge3_UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
